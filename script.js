3b415fe264ed7c8f4fe3071a3472c8dce43674c1
Egon Dohr <egondohr03@gmail.com>
2022-07-03 16:52:01 +0300

    added button interaction

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const Elements = ["rock", "paper", "scissors"];
+const choice = document.querySelectorAll('.pbuttons button')
+
 
 function computerPlay() {
     return Elements[Math.floor(Math.random() * 3)];
@@ -21,19 +23,27 @@ function playRound(playerSelection, computerSelection) {
     }
     else{
         console.log(`${playerSelection.toUpperCase()} beats ${computerSelection}`);
-        return 2;
+        return -1;
     }
 
 }
 
-
-
-
 function game(){
-    for(let i = 0; i < 5; i++) { 
-        let player = prompt("What do you pick? Rock, Paper or Scissors?");
-        playRound(player, computerPlay());
-    }
+    let count = 3, s = 0;
+    choice.forEach((el) =>
+    {
+        el.addEventListener('click', () => {
+            count--;
+            s += playRound(el.id.toString(), computerPlay());
+            if(count == 0) {
+                if(s > 0) console.log("Computer wins");
+                else if(s < 0) console.log("Player wins");
+                else console.log("Tie");
+                this.removeEventListener('click')
+            }
+        })
+    })
+    
 }
 
 game();
